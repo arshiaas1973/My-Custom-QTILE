c9224a463227a3ba8d37ad1deeabdc949f43eb7a
ScaryWWolf <55649599+arshiaas1973@users.noreply.github.com>
2024-09-27 15:51:09 +0330

    Small change

--- autostart.sh
+++ autostart.sh
@@ -2,6 +2,6 @@
 ~/scripts/wallpaper-xorg &
 picom --config ~/.config/picom/picom.conf &
 nm-applet &
-kdeconnect &
+kdeconnectd &
 hp-systray &
 setxkbmap -layout us,ir -option grp:lalt_lshift_toggle &
